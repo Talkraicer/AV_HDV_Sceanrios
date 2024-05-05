6f5fc7c5769fb625b85ba41f21b1cc936ce52f7e
Talkraicer <talkraicer@gmail.com>
2024-05-05 12:54:39 +0300

    Creating experiment VolunteerStopper + Fixing DisallowBack

--- PublicTransportV2/TraCI/utils.py
+++ PublicTransportV2/TraCI/utils.py
@@ -409,9 +409,9 @@ if __name__ == '__main__':
     AV_rates = [0.0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,1.0]
     STOP_FROM_RANGE = [900, 1000, 1100, 1200]
     STOP_TO_RANGE = [100, 200, 300]
-    # policies = ["DisallowBack" + f"_{stop_from}_{stop_to}" for stop_from in STOP_FROM_RANGE for stop_to in STOP_TO_RANGE]
-    # policies += ["Volunteer_Stopper"]
-    policies = ["Nothing"]
+    policies = ["DisallowBack" + f"_{stop_from}_{stop_to}" for stop_from in STOP_FROM_RANGE for stop_to in STOP_TO_RANGE]
+    policies += ["Volunteer_Stopper"]
+    # policies = ["Nothing"]
     parse_all_output_files(AV_rates, 1, policies)
     parse_all_pairwise(policies, AV_rates)
     # create_all_results_tables(AV_rates, policies)
