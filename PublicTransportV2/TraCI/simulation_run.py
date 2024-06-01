74db19cc7270a343812f3ac05fcd1dce7dbedfb8
Talkraicer <talkraicer@gmail.com>
2024-06-01 22:59:46 +0300

    Implementing FastLane Policy

--- PublicTransportV2/TraCI/simulation_run.py
+++ PublicTransportV2/TraCI/simulation_run.py
@@ -15,7 +15,7 @@ GUI = False
 # SIM parameters
 SIM_DURATION = 86400
 NUM_PROCESSES = 70
-POLICIES = ["FastLane"]
+POLICIES = ["Nothing"]
 STOP_FROM_RANGE = [800, 1000, 1200]
 STOP_TO_RANGE = [0,100,200]
 AV_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,1.0]
