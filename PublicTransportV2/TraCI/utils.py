67d1fec44f6f262c714611fe7f5378fa11051cc8
Talkraicer <talkraicer@gmail.com>
2024-07-01 02:29:35 +0300

    Deleting non relevant Left results and implementing LeftComp

--- PublicTransportV2/TraCI/utils.py
+++ PublicTransportV2/TraCI/utils.py
@@ -444,6 +444,7 @@ def parse_output_files_pairwise(args):
         df_rep.drop(columns=[f"{metric}_{policy_baseline}{av_rate2_dyn}" for metric in METRICS], inplace=True)
 
         df_rep["vType"] = df_rep[f"vType_{policy_name1}{av_rate}"]
+        df_rep["numPass"] = df_rep["vType"].apply(lambda x: x.split("_")[1])
         df_rep.drop(columns=[f"vType_{policy_name1}{av_rate}", f"vType_{policy_baseline}{av_rate2_dyn}"], inplace=True)
 
         df_av_rate = pd.concat([df_av_rate, df_rep])
