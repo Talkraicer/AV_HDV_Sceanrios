4c3f767c9faf6001c8f79ed01ee405496ea0e892
Talkraicer <talkraicer@gmail.com>
2024-07-03 17:02:01 +0300

    Adding wandb analysis to data

--- PublicTransportV2/TraCI/simulation_run.py
+++ PublicTransportV2/TraCI/simulation_run.py
@@ -12,7 +12,7 @@ GUI = False
 
 # SIM parameters
 SIM_DURATION = 86400
-NUM_PROCESSES = 5
+NUM_PROCESSES = 70
 AV_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 EXP_NAME_TAG = "LeftComp"
 
