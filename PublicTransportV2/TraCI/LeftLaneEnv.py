52be2a1521ff26237a30e633544bb000aef876eb
Talkraicer <talkraicer@gmail.com>
2024-08-01 22:17:12 +0300

    Implementing gym and RL learner

--- PublicTransportV2/TraCI/LeftLaneEnv.py
+++ PublicTransportV2/TraCI/LeftLaneEnv.py
@@ -1,3 +1,4 @@
+import os
 from multiprocessing import Pool
 import gymnasium as gym
 import numpy as np
@@ -7,11 +8,11 @@ from utils import handle_step, log_features, exp_name, init_wandb_logger, output
 from simulation_run import init_simulation, NUM_PROCESSES
 import traci
 from stable_baselines3 import DQN, PPO, A2C
-from stable_baselines3.common.evaluation import evaluate_policy
-
+from stable_baselines3.common.env_checker import check_env
 it_len = 10000
 num_it = 100
 NUM_EDGES = 8
+NUM_E_FEATURES = 6
 
 # DECISION VARIABLE
 MIN_NUM_PASS = 1
@@ -20,15 +21,15 @@ MIN_NUM_PASS = 1
 OBSERVATIONS = []
 
 
-def set_observations(obs_type):
+def set_observations(features_type, obs_type):
     global OBSERVATIONS
     high_limit = 0
-    if obs_type == "LOG_FEATURES":
+    if features_type == "LOG_FEATURES":
         OBSERVATIONS = ["num_total_vehs", "num_hdv_in_end_PTL", "num_vehs_in_PTL",
                         "mean_speed_in_end_PTL", "mean_speed"]
         high_limit = 100000
 
-    elif obs_type in ["E_FEATURES", "E_FEATURES_TU"]:
+    elif features_type in ["E_FEATURES", "E_FEATURES_TU"]:
         for i in range(NUM_EDGES):
             OBSERVATIONS.append(f"num_vehs_edge_{i}_no_PTL")
             OBSERVATIONS.append(f"mean_speed_edge_{i}_no_PTL")
@@ -39,8 +40,10 @@ def set_observations(obs_type):
                 OBSERVATIONS.append(f"std_speed_edge_{i}_in_PTL")
         high_limit = 200
 
-
-    return gym.spaces.Box(low=0, high=high_limit, shape=(len(OBSERVATIONS),))
+    if obs_type == "vec":
+        return gym.spaces.Box(low=0, high=high_limit, shape=(len(OBSERVATIONS),), dtype=np.float32)
+    elif obs_type == "img":
+        return gym.spaces.Box(low=0, high=255, shape=(1,NUM_E_FEATURES, NUM_EDGES), dtype=np.float32)
 
 
 def calculate_reward(env):
@@ -57,7 +60,7 @@ def calculate_reward(env):
     if df_timestep.empty:
         return 0
     total_delay = df_timestep.apply(lambda x: x["totalDelay"] * x["numPass"], axis=1).sum()
-    return -total_delay
+    return -total_delay / env.act_rate
 
 def calc_E_features(env):
     for e_idx in range(NUM_EDGES):
@@ -128,28 +131,33 @@ def action_wrapper(env, policy_name):
         for obs in OBSERVATIONS:
             location = "_".join(obs.split("_")[2:])
             if obs.startswith("num"):
-                env.state[OBSERVATIONS.index(obs)] = len(tot_speeds[location])
+                env.state[OBSERVATIONS.index(obs)] = len(tot_speeds[location])/env.act_rate
             elif obs.startswith("mean"):
-                env.state[OBSERVATIONS.index(obs)] = np.mean(tot_speeds[location])
+                env.state[OBSERVATIONS.index(obs)] = np.mean(tot_speeds[location]) if tot_speeds[location] else 0
             elif obs.startswith("std"):
-                env.state[OBSERVATIONS.index(obs)] = np.std(tot_speeds[location])
+                env.state[OBSERVATIONS.index(obs)] = np.std(tot_speeds[location]) if tot_speeds[location] else 0
 
     done = traci.simulation.getMinExpectedNumber() <= 0
 
     return calculate_reward(env), done, new_features
 
 
-ACTIONS = [lambda env: action_wrapper(env, f"StaticNumPassFL_{i}") for i in range(1, 7)]
-
-ACTION_SPACE = gym.spaces.Discrete(len(ACTIONS))
-
 
 class LeftLaneENV(gym.Env):
-    def __init__(self, policy_name, sumoCfg, log_wandb=True, features_type="LOG_FEATURES", act_rate=1):
-        self.observation_space = set_observations(features_type)
+    def __init__(self, policy_name, sumoCfg, action_space_tag="direct", log_wandb=True, features_type="LOG_FEATURES", act_rate=1):
+        self.obs_type = "img" if policy_name.split("_")[1] == "CNN" else "vec"
+
+        self.observation_space = set_observations(features_type, self.obs_type)
         self.features_type = features_type
-        self.actions = ACTIONS
-        self.action_space = ACTION_SPACE
+
+        self.action_space_tag = action_space_tag
+        self.actions = [lambda env: action_wrapper(env, f"StaticNumPassFL_{i}") for i in range(1, 7)]
+        if action_space_tag == "direct":
+            self.action_space = gym.spaces.Discrete(len(self.actions))
+        elif action_space_tag == "alter":
+            self.action_space = gym.spaces.Discrete(3)
+
+        self.last_action = None
         self.state = None
         self.timestep = 0
         self.av_rate = ".".join(sumoCfg.split("/")[-1].split(".")[:-1]).split("_")[-1][2:]
@@ -159,33 +167,54 @@ class LeftLaneENV(gym.Env):
         self.act_rate = act_rate
         if log_wandb:
             proj_tail = "_ACT_RATE_" + str(self.act_rate)
-            init_wandb_logger(self.policy_name, "av" + str(self.av_rate) + proj_tail, delete_older=True)
+            init_wandb_logger(self.policy_name, "av" + str(self.av_rate) + proj_tail+ "_" + action_space_tag, delete_older=True)
         self.log = ""
 
     def observation(self):
-        return np.array([self.state[i] for i in range(len(OBSERVATIONS))])
+        if self.obs_type == "vec":
+            return np.array([self.state[i] for i in range(len(OBSERVATIONS))])
+        elif self.obs_type == "img":
+            obs = np.zeros((1,NUM_E_FEATURES, NUM_EDGES))
+            for i in range(NUM_EDGES):
+                if 1 <= i < NUM_EDGES - 1:
+                    obs[0][0][i] = self.state[OBSERVATIONS.index(f"num_vehs_edge_{i}_in_PTL")]
+                    obs[0][1][i] = self.state[OBSERVATIONS.index(f"mean_speed_edge_{i}_in_PTL")]
+                    obs[0][2][i] = self.state[OBSERVATIONS.index(f"std_speed_edge_{i}_in_PTL")]
+                obs[0][3][i] = self.state[OBSERVATIONS.index(f"num_vehs_edge_{i}_no_PTL")]
+                obs[0][4][i] = self.state[OBSERVATIONS.index(f"mean_speed_edge_{i}_no_PTL")]
+                obs[0][5][i] = self.state[OBSERVATIONS.index(f"std_speed_edge_{i}_no_PTL")]
+            return obs
 
     def reset(self, seed=None, options=None, ):
         # check if a traci instance is already running
+        try:
+            traci.close()
+        except:
+            pass
         self.timestep = 0
         _, _, self.av_rate = init_simulation((self.policy_name, self.sumoCfg))
         self.av_rate = float(self.av_rate[2:])
         self.state = [0] * len(OBSERVATIONS)
 
-        for i in range(20):
-            ACTIONS[5](self)
-
+        for i in range(self.act_rate//1000):
+            self.actions[5](self)
+        self.last_action = 5
         return self.observation(), {}
 
     def step(self, action):
+        if self.action_space_tag == "alter":
+            if action == 0 and self.last_action > 1:
+                self.last_action -= 1
+            elif action == 2 and self.last_action < 5:
+                self.last_action += 1
+            action = self.last_action
+
         reward, done, log_msg = action_wrapper(self, "StaticNumPassFL_" + str(action + 1))
         self.log += f"Time: {self.timestep - self.act_rate} Action: {action + 1}, Reward: {reward}\n"
         if self.log_wandb and log_msg:
             log_msg["MinNumPass"] = action + 1
             log_msg["Reward"] = reward
             wandb.log(log_msg)
-        if done:
-            traci.close()
         obs = self.observation()
         return obs, reward, done, False, {}
 
@@ -195,8 +224,8 @@ class LeftLaneENV(gym.Env):
 
 
 def train_agent(cfg):
-    sumoCfg, feat_type, act_rate,agent_type = cfg
-    policy_name = agent_type + "_" + feat_type + "_ACT_RATE_" + str(act_rate)
+    sumoCfg, feat_type, act_rate,agent_type, action_space_tag = cfg
+    policy_name = agent_type + "_" + feat_type + "_ACT_RATE_" + str(act_rate)+ "_" + action_space_tag
     # Register the environment with Gym
     gym.envs.registration.register(
         id='LeftLaneENV-v0',
@@ -204,31 +233,42 @@ def train_agent(cfg):
     )
 
     env = gym.make('LeftLaneENV-v0', policy_name=policy_name, sumoCfg=sumoCfg, features_type=feat_type,
-                   act_rate=act_rate)
+                   act_rate=act_rate, action_space_tag=action_space_tag)
+    check_env(env)
     if agent_type == "DQN":
         model = DQN("MlpPolicy", env, verbose=1)
+    elif agent_type == "DQN_CNN":
+        model = DQN("CnnPolicy", env, verbose=1)
     elif agent_type == "PPO":
         model = PPO("MlpPolicy", env, verbose=1)
+    elif agent_type == "PPO_CNN":
+        model = PPO("CnnPolicy", env, verbose=1)
     elif agent_type == "A2C":
         model = A2C("MlpPolicy", env, verbose=1)
+    elif agent_type == "A2C_CNN":
+        model = A2C("CnnPolicy", env, verbose=1)
 
     for i in range(num_it):
         model.learn(total_timesteps=it_len)
 
         experiment_name = ".".join(sumoCfg.split("/")[-1].split(".")[:-1])
         agent_name = experiment_name + "_" + policy_name
-        model.save(agent_name + "_" + str(i))
+        os.makedirs("agents", exist_ok=True)
+        model.save("agents/"+agent_name + "_" + str(i))
 
     env.close()
 
 
 if __name__ == '__main__':
+
     sumoCfgs = [f"../cfg_files_LeftCompDaily/LeftCompDaily_av{r}.sumocfg" for r in [0.2, 0.4, 0.6, 0.8]]
-    agent_types = ["DQN", "PPO", "A2C"]
+    agent_types = ["DQN_CNN", "PPO_CNN", "A2C_CNN","DQN", "PPO", "A2C", ]
     feat_types = ["E_FEATURES_TU","LOG_FEATURES", "E_FEATURES"]
+    ACTION_SPACE_TAGS = ["alter"]
     act_rates = [100, 300]
-    cfgs = [(sumoCfg, feat_type, act_rate, agent_type) for sumoCfg in sumoCfgs for feat_type in feat_types
-            for act_rate in act_rates for agent_type in agent_types]
-    print("num cfgs", len(cfgs))
-    with Pool(min(NUM_PROCESSES, len(cfgs))) as pool:
-        tqdm(pool.map(train_agent, cfgs), total=len(sumoCfgs))
+    cfgs = [(sumoCfg, feat_type, act_rate, agent_type, action_space_tag) for sumoCfg in sumoCfgs for feat_type in feat_types
+            for act_rate in act_rates for agent_type in agent_types for action_space_tag in ACTION_SPACE_TAGS]
+    cfgs_clean = [cfg for cfg in cfgs if not(cfg[1] == "LOG_FEATURES" and cfg[3].endswith("CNN"))]
+    print("num cfgs", len(cfgs_clean))
+    with Pool(min(NUM_PROCESSES, len(cfgs_clean))) as pool:
+        tqdm(pool.map(train_agent, cfgs_clean), total=len(sumoCfgs))
