17eefa1b3224d3f370ec667b1ad90ef9dde79a03
Talkraicer <talkraicer@gmail.com>
2024-08-10 15:51:38 +0300

    Implementing gym and RL learner

--- PublicTransportV2/TraCI/inference_agent.py
+++ PublicTransportV2/TraCI/inference_agent.py
@@ -6,6 +6,7 @@ from stable_baselines3 import PPO, DQN, A2C
 import wandb
 from LeftLaneEnv import LeftLaneENV
 from utils import init_wandb_logger, log_features
+
 def infer(agent_path):
     agent_path_cln = agent_path.split("/")[1]
     exp_name = agent_path_cln.split("_")[0]
@@ -20,8 +21,8 @@ def infer(agent_path):
         raise ValueError("Invalid agent path")
     av_rate_str = agent_path_cln.split("_")[1]
     feat_type = "_".join(agent_path_cln.split("_")[3:6])
-    act_rate = agent_path_cln.split("_")[-3]
-    act_type = agent_path_cln.split("_")[-2]
+    act_rate = agent_path_cln.split("_")[8]
+    act_type = agent_path_cln.split("_")[9]
     sumo_cfg = f"../cfg_files_{exp_name}/{exp_name}_{av_rate_str}.sumocfg"
 
     gym.envs.registration.register(
@@ -29,7 +30,7 @@ def infer(agent_path):
         entry_point=LeftLaneENV,
     )
     env = gym.make('LeftLaneENV-v0', policy_name =agent_path_cln, sumoCfg = sumo_cfg, action_space_tag = act_type,
-                   log_wandb = False, features_type = feat_type, act_rate = int(act_rate))
+                   log_wandb = False, features_type = feat_type, act_rate = 100)
     obs, _ = env.reset()
     done = False
     init_wandb_logger(policy_name=agent_path_cln, av_rate=av_rate_str, delete_older=True)
@@ -43,7 +44,7 @@ def infer(agent_path):
         obs, reward, done, _ , _ = env.step(action)
 
 if __name__ == '__main__':
-    # paths = [f"agents/{path}" for path in os.listdir("agents") if path != "logged_agents"]
-    paths = ["agents/LeftCompDaily_av0.4_DQN_MS_EPTL_ACT_RATE_100_alter_3769.0"]
+    paths = [f"agents/{path}" for path in os.listdir("agents") if path != "logged_agents"]
+    # paths = ["agents/LeftCompDaily_av0.4_DQN_MS_EPTL_ACT_RATE_100_alter_3769.0"]
     with Pool() as p:
         p.map(infer, paths)
