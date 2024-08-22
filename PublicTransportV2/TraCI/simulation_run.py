dc1e2ac43f3ca09822a9a64ea5396fc7d10d6b26
Talkraicer <talkraicer@gmail.com>
2024-08-22 20:35:12 +0300

    fix results_utils

--- PublicTransportV2/TraCI/simulation_run.py
+++ PublicTransportV2/TraCI/simulation_run.py
@@ -23,7 +23,8 @@ EXP_NAME_TAG = "LeftCompScenarios"
 
 POLICIES = ["Plus", "Control mean_speed_in_end_PTL", "Nothing", "StaticNumPassFL"]
 # POLICIES = ["Control mean_speed_in_end_PTL"]
-CONTROL_SPEED_RANGES = [(13,20),(16,21),(16,20),(15,22),(14,22)]
+# CONTROL_SPEED_RANGES = [(13,20),(16,21),(16,20),(15,22),(14,22)]
+CONTROL_SPEED_RANGES = [(10,18),(10,20),(14,20),(16,22),(8,15),(8,20),(8,18)]
 
 # parameters for StaticNumPass
 MIN_NUM_PASS = [1, 2, 3, 4, 5]
