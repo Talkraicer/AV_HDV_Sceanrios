cc9f1bf9e2fd000198b0ad4a873d0347ecd29647
Talkraicer <talkraicer@gmail.com>
2024-08-28 18:00:05 +0300

    fix results_utils

--- PublicTransportV2/TraCI/simulation_run.py
+++ PublicTransportV2/TraCI/simulation_run.py
@@ -18,7 +18,7 @@ GUI = False
 
 # SIM parameters
 SIM_DURATION = 86400
-NUM_PROCESSES = 70
+NUM_PROCESSES = 10
 AV_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 EXP_NAME_TAG = exp_name
 
