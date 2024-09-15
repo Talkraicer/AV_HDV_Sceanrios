d82f3c4dbdf07c8125fcb7d371abf8d0baabf2eb
Talkraicer <talkraicer@gmail.com>
2024-09-15 14:29:59 +0300

    Training a model

--- PublicTransportV2/TraCI/create_dataset.py
+++ PublicTransportV2/TraCI/create_dataset.py
@@ -13,14 +13,14 @@ def handle_run(run):
             df_history["MinNumPass"] = df_history["MinPassNum"]
         X_train = df_history[features]
     except:
-        return None, None
+        return None, None, False
     X_train = X_train.dropna(subset=["MinNumPass"])
     values_to_fill = {"mean_speed_in_end_PTL": 25, "mean_speed_in_PTL": 25, "num_total_vehs": 0, "num_vehs_in_PTL": 0}
     X_train = X_train.fillna(value=values_to_fill)
     y_train = df_history["mean_pass_delay_timestamp"]
     X_train = X_train.to_numpy()[:-1]
     y_train = y_train.to_numpy()[1:]
-    return X_train, y_train
+    return X_train, y_train, True
 
 if __name__ == "__main__":
     api = wandb.Api()
@@ -34,10 +34,11 @@ if __name__ == "__main__":
         runs.extend(api.runs(f"{username}/{proj_name}"))
     results = []
     for run in tqdm(runs):
-        X,y = handle_run(run)
-        if X:
-            X_train = np.concatenate(X_train)
-            y_train = np.concatenate(y_train)
+        X,y,success = handle_run(run)
+        if success:
+            results.append((X,y))
+    X_train = np.concatenate([X for X,y in results])
+    y_train = np.concatenate([y for X,y in results])
 
     np.save("X_train.npy", X_train)
     np.save("y_train.npy", y_train)
