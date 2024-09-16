7207bb289303921fef11a4c9365737384a642af7
Talkraicer <talkraicer@gmail.com>
2024-09-17 01:10:10 +0300

    Training a model

--- PublicTransportV2/TraCI/simulation_run.py
+++ PublicTransportV2/TraCI/simulation_run.py
@@ -24,7 +24,7 @@ EXP_NAME_TAG = exp_name
 
 # POLICIES = ["Plus", "Control mean_speed_in_end_PTL", "Nothing", "StaticNumPassFL","Control mean_speed_in_PTL"]
 # POLICIES += ["ControlClipped mean_speed_in_PTL","ControlClipped mean_speed_in_end_PTL"]
-POLICIES = ["Trained_LR", "Trained_Trees"]
+POLICIES = ["Trained_LRSimFeat", "Trained_TreesSimFeat"]
 # POLICIES = ["Plus"]
 # POLICIES = ["Nothing"]
 CONTROL_SPEED_RANGES = [(13, 20), (16, 21), (16, 20), (15, 22), (14, 22)]
