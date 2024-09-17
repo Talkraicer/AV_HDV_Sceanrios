c3cd952147ad5bf8d1328320139766c0a0b49c89
Talkraicer <talkraicer@gmail.com>
2024-09-18 00:19:17 +0300

    Training a model

--- PublicTransportV2/TraCI/Training/train_regessor.py
+++ PublicTransportV2/TraCI/Training/train_regessor.py
@@ -7,9 +7,14 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor,plot_tree
 from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+import xgboost as xg
 import matplotlib.pyplot as plt
 import os
 import pickle
+
+from tqdm import tqdm
+
 av_rates = [0.1,0.2,0.3,0.4,0.6,0.8]
 features = ["mean_speed_in_end_PTL","mean_speed_in_PTL","num_total_vehs","num_vehs_in_PTL","closed","av_rate"] + ["MinNumPass"]
 experiments = ["LeftCompDaily","LeftCompScenarios","ClosedLeftCompScenarios"]
@@ -18,7 +23,7 @@ target = "mean_pass_delay_timestamp"
 
 def main():
     # load the dataset
-    dataset = pd.read_pickle("dataset.pkl")
+    dataset = pd.read_csv("dataset.csv")
 
     dataset["av_rate"] = dataset["project_name"].apply(lambda x: float(x.split("_")[-1][2:]))
     dataset["closed"] = dataset["project_name"].apply(lambda x: int("Closed" in x))
@@ -28,7 +33,7 @@ def main():
 
     # split to different datasets according to MinNumPass
     values = np.unique(X[:,-1])
-    for value in values:
+    for value in tqdm(values):
         print(f"MinNumPass = {value}")
         mask = X[:,-1] == value
 
@@ -73,6 +78,68 @@ def main():
         # save the used features
         with open(f"{LR_exp_name}/used_features.txt", "w") as f:
             f.write(",".join(features[:-1]))
+        trees_exp_name= "TreesSimFeatUnbounded"
+        model = DecisionTreeRegressor()
+        os.makedirs(trees_exp_name, exist_ok=True)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        fig = plt.figure(figsize=(25,20))
+        plot_tree(model, feature_names=features[:-1], filled=True)
+        plt.savefig(f"{trees_exp_name}/tree_{value}.png")
+        plt.show()
+        print(f"MSE DecisionTree: {mse}")
+        test_sample = X_test[0]
+        print(f"Test sample: {test_sample}")
+        print(f"Prediction: {model.predict([test_sample])}")
+        # save the model
+        pickle.dump(model, open(f"{trees_exp_name}/model_" + str(value) + ".pkl", "wb"))
+        # save the used features
+        with open(f"{trees_exp_name}/used_features.txt", "w") as f:
+            f.write(",".join(features[:-1]))
+
+        RF_exp_name = "RFSimFeat"
+        os.makedirs(RF_exp_name, exist_ok=True)
+        model = RandomForestRegressor(n_estimators=100)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"MSE RandomForest: {mse}")
+        # save the model
+        pickle.dump(model, open(f"{RF_exp_name}/model_" + str(value) + ".pkl", "wb"))
+        # save the used features
+        with open(f"{RF_exp_name}/used_features.txt", "w") as f:
+            f.write(",".join(features[:-1]))
+
+        MLPRegressor_exp_name = "MLPRegressorSimFeat"
+        os.makedirs(MLPRegressor_exp_name, exist_ok=True)
+        model = MLPRegressor(hidden_layer_sizes=(100,100), max_iter=1000)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"MSE MLPRegressor: {mse}")
+        # save the model
+        pickle.dump(model, open(f"{MLPRegressor_exp_name}/model_" + str(value) + ".pkl", "wb"))
+        # save the used features
+        with open(f"{MLPRegressor_exp_name}/used_features.txt", "w") as f:
+            f.write(",".join(features[:-1]))
+
+        xg_exp_name = "XGBoostSimFeat"
+        os.makedirs(xg_exp_name, exist_ok=True)
+        model = xg.XGBRegressor()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"MSE XGBoost: {mse}")
+        # save the model
+        pickle.dump(model, open(f"{xg_exp_name}/model_" + str(value) + ".pkl", "wb"))
+        # save the used features
+        with open(f"{xg_exp_name}/used_features.txt", "w") as f:
+            f.write(",".join(features[:-1]))
+
+
+
+
 
 if __name__ == '__main__':
     main()
